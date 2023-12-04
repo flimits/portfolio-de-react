7e2c60c3c79828c3199ecd04ef3423cb9b2ae433
Jason Savoie <flimits@gmail.com>
2023-12-03 23:23:43 -0800

    yeah, i got the portfolio card working. it should take multiple cards. now, i'll work on hoverover.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -22,51 +22,60 @@ const ContactCard = () => {
     maxWidth: '400px',
     margin: '0 auto',
     padding: '20px',
-    boxShadow: '5px 5px blue, 10px 10px red, 15px 15px green',
     borderRadius: '10px',
     backgroundColor: '#fff',
+    boxShadow: '5px 5px blue, 10px 10px red, 15px 15px green',
   };
 
   return (
     <div style={cardStyle}>
-      <h2 style={{ textAlign: 'center' }}>Contact</h2>
-      <form onSubmit={handleSubmit}>
-        <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
-          />
-        </div>
-        <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
-          />
-        </div>
-        <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-            style={{ width: '100%', padding: '8px', boxSizing: 'border-box', resize: 'vertical' }}
-          />
-        </div>
+      {submitted ? (
         <div style={{ textAlign: 'center' }}>
-          <button type="submit" style={{ padding: '10px 20px', background: '#007BFF', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Submit</button>
+          <h2>Thank You!</h2>
+          <p>Your message has been successfully submitted.</p>
         </div>
-      </form>
+      ) : (
+        <>
+          <h2 style={{ textAlign: 'center' }}>Contact</h2>
+          <form onSubmit={handleSubmit}>
+            <div style={{ marginBottom: '15px' }}>
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
+              />
+            </div>
+            <div style={{ marginBottom: '15px' }}>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
+              />
+            </div>
+            <div style={{ marginBottom: '15px' }}>
+              <label htmlFor="message">Message:</label>
+              <textarea
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+                style={{ width: '100%', padding: '8px', boxSizing: 'border-box', resize: 'vertical' }}
+              />
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <button type="submit" style={{ padding: '10px 20px', background: '#007BFF', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Submit</button>
+            </div>
+          </form>
+        </>
+      )}
     </div>
   );
 };
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,20 +1,32 @@
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-export default function Portfolio() {
 
+import project1Image from '../images/gonuts_donuts.png'
+
+export default function Portfolio() {
+  const portfolios = [
+    {
+      title: 'Project 1',
+      description: 'This project focused on making the best donut finder site in town, Complete with recipes and search',
+      myImage: project1Image,
+      link: 'https://github.com/flimits/gonutzfordonutz'
+    }
+  ]
+  // What this below is doing is the xs helps small screens to display in columns.
+  // The md is for medium size screens and will display 2 columns on the screen.
+  // g-4 is a react spacing called gutter and sets spacing between items
   return (
     <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
+      {portfolios.map((item, idx) => (
         <Col key={idx}>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={item.myImage} alt={`Image for ${item.title}`} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>{item.title}</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                {item.description}
               </Card.Text>
             </Card.Body>
           </Card>
