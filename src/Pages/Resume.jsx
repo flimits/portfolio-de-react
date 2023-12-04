57f811e0ad4d7939a58aa6320b7ecb325d3e952f
Jason Savoie <flimits@gmail.com>
2023-12-04 09:02:27 -0800

    fixing up the resume portion. cleaned up unused files

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,10 +1,36 @@
+import React from 'react';
+
 export default function Resume() {
     return (
         <div>
             <h1>Resume De Savoie</h1>
-            <p>
-                Here is my experience
-            </p>
+            <p>Here is my experience</p>
+            <a href="/jason_savoie_resume.pdf" download>
+                You can Download My Resume Here
+            </a>
+            <br />
+            <br />
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <h2>Front-End Skills</h2>
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Bootstrap</p>
+                        <p>Javascript</p>
+                        <p>React</p>
+                    </div>
+                    <div className="col-6">
+                        <h2>Back-End Skills</h2>
+                        <p>APIs</p>
+                        <p>ExpressJs</p>
+                        <p>MySql Sequalize</p>
+                        <p>Mongoose</p>
+                        <p>GraphQL</p>
+                        <p>Node</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
